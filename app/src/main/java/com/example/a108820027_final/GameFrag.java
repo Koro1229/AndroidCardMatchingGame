3862bf14389b7964a8f01b29eeebc92f95099c8f
Koro <youcry987951@gmail.com>
2022-05-31 02:11:34 +0900

    fail on using gridLayout

--- app/src/main/java/com/example/a108820027_final/GameFrag.java
+++ app/src/main/java/com/example/a108820027_final/GameFrag.java
@@ -5,15 +5,26 @@ import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +37,10 @@ public class GameFrag extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private GridLayout mGridLayout;
+    private static final int N = 4;
+    private final List<FrameLayout> gameCards = new ArrayList<FrameLayout>();
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -72,7 +87,10 @@ public class GameFrag extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState ) {
         super.onViewCreated(view, savedInstanceState);
-
+        mGridLayout = getView().findViewById(R.id.gameContent);
+//        mGridLayout.removeAllViews();
+        mGridLayout.setColumnCount(N);
+        mGridLayout.setRowCount(N);
         Button homeButton = getView().findViewById(R.id.homeButton);
         Button revealButton = getView().findViewById(R.id.revealButton);
 
@@ -95,6 +113,16 @@ public class GameFrag extends Fragment {
                 ft.commit();
             }
         });
+
+//        for(int r = 0; r < N; r++){
+//            for(int c = 0; c < N; c++){
+//                createGridCardView(getContext(), r, c);
+//            }
+//        }
+//        createGridCardView(getContext(), 0, 0);
+//        createGridCardView(getContext(), 0, 1);
+//        createGridCardView(getContext(), 1, 0);
+//        createGridCardView(getContext(), 1, 1);
     }
 
 
@@ -117,5 +145,71 @@ public class GameFrag extends Fragment {
                 inVisibleView.setVisibility(View.GONE);
             }
         });
+    }
+
+
+
+    private FrameLayout getFrameLayout(int r, int c){
+        int index = r * N + c;
+        return gameCards.get(index);
+    }
+
+    private void createGridCardView(Context context, int row, int col){
+
+
+//        GridLayout.LayoutParams gridParams = new GridLayout.LayoutParams();
+//        gridParams.columnSpec = GridLayout.spec(col);
+//        gridParams.rowSpec = GridLayout.spec(row);
+
+//生成FrameLayout
+        FrameLayout frameLayout = new FrameLayout(context);
+        FrameLayout.LayoutParams frameParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        frameParams.gravity = Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL;
+        frameLayout.setLayoutParams(frameParams);
+
+//生成card背面
+        CardView back = new CardView(context);
+        ViewGroup.MarginLayoutParams cardParams = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        cardParams.setMargins(32, 32, 32, 32);
+        back.setLayoutParams(cardParams);
+        back.setForegroundGravity(Gravity.FILL);
+        back.setCardBackgroundColor(getResources().getColor(R.color.teal_200));
+
+        GradientDrawable shape = new GradientDrawable();
+        shape.setCornerRadius(64);
+        shape.setColor(getResources().getColor(R.color.teal_200));
+        back.setBackground(shape);
+
+//生成Card裡面的內容(LinearLayout)
+        LinearLayout linearLayout = new LinearLayout(context);
+        LinearLayout.LayoutParams linearParams = new LinearLayout.LayoutParams((int)(getResources().getDimension(R.dimen.card_linear_size)), (int)(getResources().getDimension(R.dimen.card_linear_size)));
+        linearParams.gravity = Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL;
+        linearLayout.setLayoutParams(linearParams);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        ImageView imageView = new ImageView(context);
+        ViewGroup.LayoutParams imageParams = new ViewGroup.LayoutParams(R.dimen.card_image_size, R.dimen.card_image_size);
+        imageView.setLayoutParams(imageParams);
+        imageView.setImageResource(R.drawable.ic_launcher_foreground);
+
+        TextView textView = new TextView(context);
+        ViewGroup.LayoutParams textParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        textView.setLayoutParams(textParams);
+        textView.setText(R.string.card_back_text);
+        textView.setTextColor(getResources().getColor(R.color.white));
+        textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+//內容加入到一個LinearLayout
+        linearLayout.addView(imageView);
+        linearLayout.addView(textView);
+
+//        View l = getLayoutInflater().from(context).inflate(linearLayout, null);
+//加入Card
+        back.addView(linearLayout);
+//加入FrameLayout
+        frameLayout.addView(back);
+//加入GridLayout
+        mGridLayout.addView(frameLayout, new GridLayout.LayoutParams(
+                GridLayout.spec(row, GridLayout.CENTER), GridLayout.spec(col, GridLayout.CENTER)));
+
     }
 }
